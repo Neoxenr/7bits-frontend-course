465b970c4df2f8e734759977c50d940871612c11
Ivan Dolgushin <iv.dolgushin@gmail.com>
2021-11-30 13:41:45 +0600

    Practice 4

---
+++ 04-base-practice/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+
+function concatCssAndCopy() {
+  return gulp
+    .src("src/styles/*.css")
+    .pipe(concat("styles/style.css"))
+    .pipe(gulp.dest("build/"));
+}
+
+function copyHtml() {
+  return gulp.src("src/templates/*.html").pipe(gulp.dest("build/templates"));
+}
+
+exports.default = gulp.parallel(concatCssAndCopy, copyHtml);
